80a3a53d8bc9fc7bd4570ff730d239b31baab4f4
Pamela <pamela@ejemplo.com>
2016-02-01 21:25:47 -0600

    added viws to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post #"""El punto después de from indica el directorio actual o la aplicación actual. 
-#Como views.py y models.py están en el mismo directorio, simplemente usamos . y el nombre del archivo (sin .py).
+from django.shortcuts import render, get_object_or_404 #Como views.py y models.py están en el mismo directorio, simplemente usamos . y el nombre del archivo (sin .py).
 #Ahora importamos el nombre del modelo (Post)."""
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request,'blog/post_list.html', {'posts': posts})
+	
+def post_detail(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
